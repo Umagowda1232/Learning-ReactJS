3f982c4bf0fe8921be3dcab67dddd243e8a7575f
Umagowda1232 <umavgowda22@gmail.com>
2025-05-25 17:27:39 +0530

    3.16 Outputting list data dynamically using map()

--- Module-3/src/App.jsx
+++ Module-3/src/App.jsx
@@ -43,14 +43,19 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
+          {CORE_CONCEPTS.map((conceptItem) => (
+            <CoreConcept key={conceptItem.title} {...conceptItem} />
+          ))}
+
+          {/* 
           <CoreConcept 
             title={CORE_CONCEPTS[0].title}
             description={CORE_CONCEPTS[0].description}
             image={CORE_CONCEPTS[0].image}
           />
           <CoreConcept {...CORE_CONCEPTS[1]}
-          />  {/* We can use like this too
-           we get same results */}
+          />  We can use like this too
+           we get same results 
 
           <CoreConcept 
             title={CORE_CONCEPTS[2].title}
@@ -62,6 +67,8 @@ function App() {
             description={CORE_CONCEPTS[3].description}
             image={CORE_CONCEPTS[3].image}
           />
+          */ }
+          
 
         </ul>
         </section>
