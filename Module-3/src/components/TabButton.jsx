8036e16f8ec54e8bf68b9ec81368bcdd3244cf73
Umagowda1232 <umavgowda22@gmail.com>
2025-05-25 08:44:43 +0530

    3.8 Reacting to events

--- Module-3/src/components/TabButton.jsx
+++ Module-3/src/components/TabButton.jsx
@@ -1,3 +1,9 @@
-export default function TabButton (props) {
-    return <li><button>{props.children}</button></li>
+export default function TabButton ({children}) {
+   function handleClick() {
+    console.log('Hello world');
+   }
+    return (<li>
+        <button onClick={handleClick}>{children}</button>
+        </li>
+    );
 }
