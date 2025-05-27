606ea57300d0450fd3799f3b8038e35d1be0688a
Umagowda1232 <umavgowda22@gmail.com>
2025-05-27 23:06:40 +0530

    Concept Repetition:Working with State, Component instances work in isolation

--- Module-4_TicTakToe/src/App.jsx
+++ Module-4_TicTakToe/src/App.jsx
@@ -1,15 +1,18 @@
-import Player from "./components/Player.jsx";
+import Player from './components/Player.jsx';
 
 function App() {
-   return <main>
+  return (
+    <main>
       <div id="game-container">
         <ol id="players">
-          < Player name="Player 1" symbol="X" />
-          < Player name="Player 2" symbol="O" />
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
         </ol>
         GAME BOARD
       </div>
-   </main>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
