911bd817d829d92579dff8f669e3089c5d61086c
Umagowda1232 <umavgowda22@gmail.com>
2025-05-27 20:27:13 +0530

    4.2 Problem:Props are not forwarded to inner elements

--- Module-4/src/components/Examples.jsx
+++ Module-4/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import TabButton from './TabButton.jsx';
+import Section from './Section.jsx';
 import { EXAMPLES } from '../data.js';
 
 
@@ -28,8 +29,7 @@ export default function Examples() {
     );
   }
     return (
-       <section id="examples">
-          <h2>Examples</h2>
+       <Section title="Examples" id="examples">
           <menu>
             <TabButton
               isSelected={selectedTopic === 'components'}
@@ -57,7 +57,7 @@ export default function Examples() {
             </TabButton>
           </menu>
           {tabContent}
-        </section>
+        </Section>
     );
 }
 
